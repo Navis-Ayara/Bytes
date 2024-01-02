2b4a3dd78e20743022e4a9aa7a9d9e77618f7ddd
makobiziles <makofiamike2@gmail.com>
2024-01-02 19:19:14 +0300

    Second reset

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask,render_template,request,redirect,url_for,sessions,session
+from database import get_database
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+app = Flask(__name__)
+app.run['SECRET_KEY'] = os.urandom(24)
+
+def get_current_user():
+    user = None
+    if 'user' in session:
+        user = session['user']
+        db = get_database()
+        user_cusor = db.execute("select * from users where username = ?", [user])
+        user = user_cusor.fetchone()
+    return user
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        # get form data
+        username = request.form['username']
+        user_enter_password = request.form['password']
+        db = get_database()
+        cursor = db.execute('SELECT * FROM users WHERE username = ?', [username])
+        user = cursor.fetchone()
+        
+        if user:
+            if check_password_hash(user['password'], user_enter_password):
+                session['user'] = user['username']
+                return redirect(url_for('home'))
+            else:
+                error = 'Invalid username or password'
+    return render_template('login.html', loginerror=error)
+       
+
+
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+    register_error = None
+    if request.method == 'POST':
+        # get form data
+        username = request.form['username']
+        password = request.form['password']
+
+        hashed_password = generate_password_hash(password)
+
+        db = get_database()
+        #duplicate check    
+        user_cusor = db.execute("select * from users where username = ?", [username])
+        existing_user = user_cusor.fetchone()
+
+        if existing_user:
+            register_error = 'Username already exists'
+            return render_template('register.html', register_error=register_error)
+
+        db.execute('INSERT INTO users (username, password) VALUES (?, ?)', [username, password],'0')
+        db.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', register_error=register_error)
+
+
+@app.route('/logout')
+def logout():
+    return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
