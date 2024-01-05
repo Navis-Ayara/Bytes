d4ee601d4f0771a44a80f54a39abc4b0d69fb81a
makobiziles <makofiamike2@gmail.com>
2024-01-05 18:57:14 +0300

    Wow had a major refresh

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
-    
+    path('bloggets/',include('django.contrib.auth.urls')),
+    path('bloggets/',include ('bloggets.urls')),
 ]
