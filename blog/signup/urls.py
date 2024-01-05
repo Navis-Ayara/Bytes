2d282b4702ec379e965a4366a3d6907cdff45427
makobiziles <makofiamike2@gmail.com>
2024-01-05 17:05:27 +0300

    test 2

--- blog/signup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import User_SignUp
-
-##### path goes here
-
-urlpatterns = [
-    path('registration/', User_SignUp.as_view(), name='registration'), # signup/path 
-] 
