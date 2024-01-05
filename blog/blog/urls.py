32879a282fde89f0ade3f6d4ca327eb5b83f0662
makobiziles <makofiamike2@gmail.com>
2024-01-05 17:07:08 +0300

    1

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
-    path('/signup', include('django.contrib.auth.urls')),
-    path('/signup', include('signup.urls')),
+    
 ]
