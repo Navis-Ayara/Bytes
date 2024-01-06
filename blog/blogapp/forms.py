203ce149e7da73934ee1b48a6d3647afb96f2726
Navis Ayara <navisayara@gmail.com>
2024-01-06 14:10:22 +0300

    Update forms.py
    Fixed

--- blog/blogapp/forms.py
+++ blog/blogapp/forms.py
@@ -1,45 +1,24 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django import forms
 from .models import Post
-from .forms import PostForm, EditForm
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-#def home(request):
-#   return render(request, 'home.html', {})
+#Classes
 
-class Home(ListView):
-    model = Post
-    template_name = 'home.html'
-    #ordering = ['-id']
-    ordering = ['-post_date']
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','author','body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Enter Title'}),
+            'author': forms.Select(attrs={'class': 'form-control','placeholder': 'Enter Author'}),
+            'body': forms.Textarea(attrs={'class': 'form-control','placeholder': 'What you thinking ?'}),
+        }
 
 
-class article_detail(DetailView):
-    model = Post
-    template_name = 'article-details.html'
-  
-
-class Add_Post(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'New_post.html'
-    login_url = "/signup/login"
-    #fields = '__all__'
-    #fields = ('title','author','body')
-
-class Update_Post(LoginRequiredMixin, UpdateView):
-    model = Post
-    form_class = EditForm
-    template_name = 'edit-post.html'
-    login_url = "/signup/login"
-    #fields = '__all__'
-    #fields = ('title','body')    
-
-class Delete_Post(LoginRequiredMixin, DeleteView):
-    model = Post
-    template_name = 'delete-post.html'
-    login_url = "/signup/login"
-    success_url = reverse_lazy('home')
-    redirct_field_name = 'home'
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
