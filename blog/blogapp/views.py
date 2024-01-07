68d1e8d2a28cb2306a67c985268710f2d952e8e9
Navis Ayara <navisayara@gmail.com>
2024-01-07 09:46:34 +0300

    Co-authored-by: makobiziles <makobiziles@users.noreply.github.com>

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,41 +1,42 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-#def home(request):
-#   return render(request, 'home.html', {})
 
 class Home(ListView):
     model = Post
     template_name = 'home.html'
-    #ordering = ['-id']
     ordering = ['-post_date']
 
 
 class article_detail(DetailView):
     model = Post
     template_name = 'article-details.html'
-  
+
 
 class Add_Post(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'New_post.html'
     login_url = "/signup/login"
-    #fields = '__all__'
-    #fields = ('title','author','body')
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 class Update_Post(LoginRequiredMixin, UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'edit-post.html'
     login_url = "/signup/login"
-    #fields = '__all__'
-    #fields = ('title','body')    
+
 
 class Delete_Post(LoginRequiredMixin, DeleteView):
     model = Post
@@ -43,3 +44,8 @@ class Delete_Post(LoginRequiredMixin, DeleteView):
     login_url = "/signup/login"
     success_url = reverse_lazy('home')
     redirct_field_name = 'home'
+
+@login_required
+def logout_view(request):
+  logout(request)
+  return redirect('home')
